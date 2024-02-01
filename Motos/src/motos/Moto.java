65d552f3f43ecfa1ff89ed0070a97b4a0a928a0d
Wallisson Alves <wallissonlima008@gmail.com>
2024-02-01 00:58:29 -0300

    Add atributes and especials methods.

--- Motos/src/motos/Moto.java
+++ Motos/src/motos/Moto.java
@@ -1,13 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package motos;
-
-/**
- *
- * @author Wallisson
- */
-public class Moto {
+public abstract class Moto {
     
+    // ATRIBUTOS ===============================================================
+    private float cilindradas;
+    private String tipomotor;
+    private String combustivel;
+    private String transmissao;
+    private String partida;
+    private float peso;
+    private String rodas;
+    private float captanque;
+    private String farol;
+    
+    // MÉTODOS PRINCIPAIS ======================================================
+    
+    
+    // MÉTODOS ESPECIAIS =======================================================
+    public float getCilindradas(){
+        return cilindradas;
+    }
+    public void setCilindradas(float cilindradas){
+        this.cilindradas = cilindradas;
+    }
+    public String getTipoMotor(){
+        return tipomotor;
+    }
+    public void setTipoMotor(String tipomotor){
+        this.tipomotor = tipomotor;
+    }
+    public String getCombustivel(){
+        return combustivel;
+    }
+    public void setCombustivel(String combustivel){
+        this.combustivel = combustivel;
+    }
+    public String getTransmissao(){
+        return transmissao;
+    }
+    public void setTransmissao(String transmissao){
+        this.transmissao = transmissao;
+    }
+    public String getPartida(){
+        return partida;
+    }
+    public void setPartida(String partida){
+        this.partida = partida;
+    }
+    public float getPeso(){
+        return peso;
+    }
+    public void setPeso(float peso){
+        this.peso = peso;
+    }
+    public String getRodas(){
+        return rodas;
+    }
+    public void setRodas(String rodas){
+        this.rodas = rodas;
+    }
+    public float getCapTanque(){
+        return captanque;
+    }
+    public void setCapTanque(float captanque){
+        this.captanque = captanque;
+    }
+    public String getFarol(){
+        return farol;
+    }
+    public void setFarol(String farol){
+        this.farol = farol;
+    }
 }
